cea7a2ea794f4033278311b65474c9697b72e7ae
Alisa Siegmund <alisa.siegmund@gmail.com>
2019-11-19 11:55:20 +0100

    added validations for user (first/last_name combination) and tested the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates_uniqueness_of :first_name, scope: :last_name
 end
