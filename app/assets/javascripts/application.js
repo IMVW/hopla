23525dfb3223dc9f1f453aa58183b7984906abee
Siya Ndiki <siyandiki@gmail.com>
2019-11-26 10:52:03 +0100

    weather app with username glitch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,18 @@
-//= require rails-ujs
-//= require_tree .
+// //= require rails-ujs
+// //= require_tree .
+// const API_KEY = 'e4fd94634ae2ad4aa1c76d8e4d0cd626'
+// const Weather = () =>{
+//   fetch(`http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${API_KEY}`)
 
-document.getElementById('td-0').addEventListner('click',function(){
-  document.querySelector('bg-modal').style.display ='flex'
-})
+//   .then(response => response.json())
+//   .then((data) => {
+
+//     console.log(data);
+//     console.log(data.name);
+//     console.log(data.main.temp);
+//     console.log(data.weather[0].description);
+//   })
+
+// }
 
 
