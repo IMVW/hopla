dc11b34783959a652eb0a5d673357fa1d1c193bb
Siya Ndiki <siyandiki@gmail.com>
2019-11-27 12:41:04 +0100

    changed the weather and greeting

--- app/javascript/weatherapp.js
+++ app/javascript/weatherapp.js
@@ -20,7 +20,8 @@ const Weather = () => {
 
     const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
     let current_datetime = new Date()
-    let formatted_date = current_datetime.getDate() + " " + months[current_datetime.getMonth()] + "         " + current_datetime.getFullYear()
+    // let formatted_date = current_datetime.getDate() + " " + months[current_datetime.getMonth()] + "         " + current_datetime.getFullYear()
+    let formatted_date = months[current_datetime.getMonth()] +" "+current_datetime.getDate()
     console.log(formatted_date)
 
     document.querySelector('#city').innerHTML = city
